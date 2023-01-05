de05ee6ac2df481315703b42492a19cd68c34aca
Alex <Alexander.evtukh@yandex.ru>
2023-01-05 18:18:47 +0300

    added waiter for elements

--- src/main/java/utils/Waiter.java
+++ src/main/java/utils/Waiter.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Waiter {
     private static final int WAIT_TIMEOUT_SECONDS = 5;
@@ -15,5 +16,10 @@ public class Waiter {
     public static WebElement waitVisibilityOfElement(WebElement element) {
         return new WebDriverWait(DriverSingleton.getInstance().getDriver(), Duration.ofSeconds(WAIT_TIMEOUT_SECONDS)) //маг число
                 .until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static List<WebElement> waitVisibilityOfElements(List<WebElement> element) {
+        return new WebDriverWait(DriverSingleton.getInstance().getDriver(), Duration.ofSeconds(WAIT_TIMEOUT_SECONDS)) //маг число
+                .until(ExpectedConditions.visibilityOfAllElements(element));
     }
 }
